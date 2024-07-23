2566d8f6cb1066b985ab4cf64125299b667f5d3c
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-07-23 12:53:59 +1000

    Added __init__.py to reduce module import redundancy

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+# ENVIRONMENT::
+import sys
+from pathlib import Path
+
+PYTHONPATH = Path(__file__).parents[1].__str__()
+if PYTHONPATH not in sys.path:
+    sys.path.append(PYTHONPATH)
+
+# MODULES::
+from models import utils
+from models import model
