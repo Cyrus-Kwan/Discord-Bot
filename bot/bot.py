3c2000b54de10df41018c1b63ea77a4168163c3c
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-07-23 12:59:00 +1000

    Moved bot login to its own method

--- bot/bot.py
+++ bot/bot.py
@@ -18,21 +18,29 @@ class Bot(discord.Client):
         print(f"Logged in as: {self.user}")
         return self.user
 
-def main():
-    # Bot configuration
-    intents = discord.Intents.all()
-    bot = Bot(intents=intents)
+    async def on_message(self, message:discord.Message):
+        if message.author == self.user:
+            return
 
+        await message.channel.send()
+
+def get_token(bot_name:str) -> str:
     # Access the database
     model = Model("bot_config.db")
     config = model.schema["bot_config"]
 
     # Parameters to get the access token for the specified bot
-    bot_name = "cmd#7080"
     bot_token = config[config["bot_name"] == bot_name]["access_token"]
 
+    return bot_token[0]
+
+def main():
+    # Bot configuration
+    intents = discord.Intents.all()
+    bot = Bot(intents=intents)
+
     # Run the bot using private access token
-    bot.run(*bot_token)
+    bot.run(get_token("cmd#7080"))
 
 if __name__ == "__main__":
     main()
