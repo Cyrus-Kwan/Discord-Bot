f091468ccebf7644a78ce500466f9a46fee60b21
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-07-28 23:14:16 +1000

    bot now supports async/await initialization of models class. Also slash commands are broken...

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,9 @@
 # PACKAGES::
 import discord
+from discord.ext import commands
 from discord import app_commands
 import pandas as pd
+import asyncio
 
 # ENVIRONMENT::
 import sys
@@ -15,33 +17,30 @@ if PYTHONPATH not in sys.path:
 from models.model import Model
 
 class Bot(discord.Client):
-    def __init__(self, bot_name:str, intents:discord.Intents):
+    def __init__(self, intents:discord.Intents):
         super().__init__(intents=intents)
 
         # App and slash command support
         self.tree = app_commands.CommandTree(self)
 
-        # Run the bot using private access token
-        self.run(get_token(bot_name=bot_name))
-
     async def on_ready(self) -> discord.Client:
         print(f"Logged in as: {self.user}")
+
         return self.user
 
-    # TODO: Figure out why this works
     async def setup_hook(self) -> None:
         # Register commands here
         @self.tree.command(name="echo", description="Echoes a message.")
-        @app_commands.describe(message="The message to echo.")
+        @app_commands.describe(message="Message to echo.")
         async def echo(interaction:discord.Interaction, message:str) -> None:
             await interaction.response.send_message(message)
 
         # Sync the application commands with the server
         await self.tree.sync()
 
-def get_token(bot_name:str) -> str:
+async def get_token(bot_name:str) -> str:
     # Access the database
-    model = Model("bot_config.db")
+    model = await Model.create("bot_config.db")
     config = model.schema["bot_config"]
 
     # Parameters to get the access token for the specified bot
@@ -49,10 +48,11 @@ def get_token(bot_name:str) -> str:
 
     return bot_token[0]
 
-def main():
+if __name__ == "__main__":
     # Bot configuration
     intents = discord.Intents.all()
-    bot = Bot(bot_name="cmd#7080", intents=intents)
+    bot = Bot(intents=intents)
+    token = asyncio.run(get_token(bot_name="cmd#7080"))
 
-if __name__ == "__main__":
-    main()
+    # Run the bot using private access token
+    bot.run(token)
