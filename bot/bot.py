b444127bc0e54f7e0391c37a51891f8464e5b62c
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-07-29 17:33:23 +1000

    renamed parameters for clarity

--- bot/bot.py
+++ bot/bot.py
@@ -17,7 +17,7 @@ if PYTHONPATH not in sys.path:
 from models.model import Model
 import commands
 
-class Bot(discord.Client):
+class Client(discord.Client):
     def __init__(self, intents:discord.Intents):
         super().__init__(intents=intents)
 
@@ -27,30 +27,32 @@ class Bot(discord.Client):
     async def on_ready(self) -> discord.Client:
         print(f"Logged in as: {self.user}")
 
-        # return self.user
+        return self.user
 
     async def setup_hook(self) -> None:
         # Register commands here
         commands.Echo(self)
+        commands.Users(self)
+        commands.Shutdown(self)
 
         # Sync the application commands with the server
         await self.tree.sync()
 
-async def get_token(bot_name:str) -> str:
+async def get_token(client_name:str) -> str:
     # Access the database
-    model = await Model.create("bot_config.db")
-    config = model.schema["bot_config"]
+    model = await Model.create("config.db")
+    config = model.schema["clients"]
 
     # Parameters to get the access token for the specified bot
-    bot_token = config[config["bot_name"] == bot_name]["access_token"]
+    client_token = config[config["client_name"] == client_name]["access_token"]
 
-    return bot_token[0]
+    return client_token[0]
 
 if __name__ == "__main__":
     # Bot configuration
     intents = discord.Intents.all()
-    bot = Bot(intents=intents)
-    token = asyncio.run(get_token(bot_name="cmd#7080"))
+    client = Client(intents=intents)
+    token = asyncio.run(get_token(client_name="cmd#7080"))
 
     # Run the bot using private access token
-    bot.run(token)
+    client.run(token)
