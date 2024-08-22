b90b39d2c731024374f84fafd8f1861d567d1d8f
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-08-22 22:57:19 +1000

    Fixed bug where inject wasn't able to handle entire scripts

--- models/model.py
+++ models/model.py
@@ -7,7 +7,7 @@ import asyncio
 import re
 
 # ENVIRONMENT::
-PYTHONPATH = Path(__file__).parents[2].__str__()
+PYTHONPATH = Path(__file__).parents[1].__str__()
 if PYTHONPATH not in sys.path:
     sys.path.append(PYTHONPATH)
 
@@ -18,8 +18,8 @@ from models import *
 class Model():
     # Constructor
     def __init__(self, database:str):
-        self.connection: sqlite3.Connection = self.database_connect(database)
-        self.cursor: sqlite3.Connection.cursor = self.connection.cursor()
+        self.connection:sqlite3.Connection = self.database_connect(database)
+        self.cursor:sqlite3.Connection.cursor = self.connection.cursor()
 
     @classmethod
     async def create(cls, database:str, script:str=None):
@@ -38,8 +38,8 @@ class Model():
         '''
         Creates a connection to the specified database file.
         '''
-        database_dir: pathlib.WindowsPath = Path(__file__).parent / "databases"
-        connection: sqlite3.Connection = sqlite3.connect(database_dir / database)
+        database_dir:pathlib.WindowsPath = Path(__file__).parent / "databases"
+        connection:sqlite3.Connection = sqlite3.connect(database_dir / database)
 
         return connection
 
@@ -48,13 +48,13 @@ class Model():
         '''
         Returns the schema of the entire database as a map containing each table to their names.
         '''
-        sql: str = "SELECT name FROM sqlite_master WHERE type='table';"
-        selection: list[tuple] = self.cursor.execute(sql).fetchall()
-        table: pd.DataFrame = await self.read(sql)
-        table_names: pd.Series = table["name"]
+        sql:str = "SELECT name FROM sqlite_master WHERE type='table';"
+        selection:list[tuple] = self.cursor.execute(sql).fetchall()
+        table:pd.DataFrame = await self.read(sql)
+        table_names:pd.Series = table["name"]
 
         # Dictionary containing the tables in the database stored as pandas dataframes
-        schema: dict[pd.DataFrame] = {}
+        schema:dict[pd.DataFrame] = {}
         for table in table_names:
             sql = f"SELECT * FROM {table};"
             schema[table] = await self.read(sql)
@@ -64,12 +64,12 @@ class Model():
     async def primary_key(self, table:str) -> pd.Series:
         with self.connection:
             # Query the PRAGMA table_info for the specified table
-            sql: str = f"PRAGMA table_info({table})"
+            sql:str = f"PRAGMA table_info({table})"
             self.cursor.execute(sql)
 
             # PRAGMA table
             column_names = [description[0] for description in self.cursor.description]
-            columns_info: list[tuple] = self.cursor.fetchall()
+            columns_info:list[tuple] = self.cursor.fetchall()
 
             # Dataframe representation
             pragma = pd.DataFrame(data=columns_info, columns=column_names)
@@ -94,11 +94,11 @@ class Model():
             return None
 
         with self.connection:
-            columns: list[str] = []
+            columns:list[str] = []
             for value in self.cursor.execute(query).description:
                 columns.append(value[0])
 
-            sql = pd.read_sql_query(query, self.connection)
+            sql:str = pd.read_sql_query(query, self.connection)
             select = pd.DataFrame(sql, columns=columns)
 
         return select
@@ -140,10 +140,8 @@ class Model():
 
         script_path = Path(*models.scripts.__path__)
         with open(f"{script_path}/{script}") as content:
-            sql = content.read()
-            for query in sql.split(";")[:-1]:
-                await self.write(query)
-
+            sql:str = content.read()
+            self.cursor.executescript(sql)
         return
 
     async def upsert(self, table:str, values:dict) -> None:
@@ -153,12 +151,12 @@ class Model():
         schema = await self.get_schema()
         columns = schema[table].columns
 
-        primary_key: str = ", ".join(await self.primary_key(table=table))
-        column_names: str = ", ".join(columns)
-        pointers: str = ", ".join(f":{col}" for col in columns)
-        update_clause: str = ", ".join(f"{col}=excluded.{col}" for col in columns)
+        primary_key:str = ", ".join(await self.primary_key(table=table))
+        column_names:str = ", ".join(columns)
+        pointers:str = ", ".join(f":{col}" for col in columns)
+        update_clause:str = ", ".join(f"{col}=excluded.{col}" for col in columns)
         
-        sql: str = f"""
+        sql:str = f"""
         INSERT INTO {table} ({column_names}) VALUES ({pointers})
         ON CONFLICT ({primary_key}) DO UPDATE SET {update_clause}
         """
@@ -166,14 +164,35 @@ class Model():
 
         return
 
+    async def has(self, table:str, column:str, value) -> bool:
+        '''
+        Returns a true false depending on if a certain value is contained in the specified column
+        '''
+        sql:str = f"""
+        SELECT EXISTS(
+            SELECT DISTINCT {column} FROM {table}
+            WHERE {column} = :value
+            )
+        """
+
+        param:dict = {
+            "value":value
+            }
+
+        query = self.cursor.execute(sql, param)
+        fetch = query.fetchone()
+        result = True in fetch
+
+        return result
+
 async def main():
-    mod = await Model.create("main.db")
-    sql = f"""
-    SELECT content 
-    FROM messages
-    WHERE channel_id = 1272532155641233439 
-    ORDER BY date_created DESC;
+    mod = await Model.create("ngram.db", "ngram.sql")
+    sql = """
+    SELECT * FROM Graph;
     """
+    await mod.write("INSERT INTO Graph (Current, Previous, Edge) VALUES ('a b', 'c d', 1);")
+    await mod.write("INSERT INTO Graph (Current, Previous, Edge) VALUES ('a b', 'e f', 1);")
+
     print(await mod.read(sql))
 
 if __name__ == "__main__":
