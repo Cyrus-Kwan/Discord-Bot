add58bedc7adef49be6738a29aec652d5696a43f
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-08-22 22:58:03 +1000

    WIP triggers to calculate probability on insert or update

--- models/scripts/ngram.sql
+++ models/scripts/ngram.sql
@@ -1,7 +1,39 @@
 --Table that stores substring states as an adjacency list
-CREATE TABLE IF NOT EXISTS graph (
-    current TEXT,
+CREATE TABLE IF NOT EXISTS Graph (
+    Current TEXT,
     previous TEXT,
-    edge INTEGER,
-    probability DECIMAL(size, 2)
-)
+    Edge INTEGER,
+    Probability REAL(2),
+    PRIMARY KEY (Current, previous)
+);
+
+DROP TRIGGER IF EXISTS Probability_on_insert;
+DROP TRIGGER IF EXISTS Probability_on_update;
+
+CREATE TRIGGER Probability_on_insert
+AFTER INSERT ON Graph
+FOR EACH ROW
+BEGIN
+    -- Update the probability for all rows with the same current node
+    UPDATE Graph
+    SET Probability = Edge / (
+        SELECT SUM(Edge)
+        FROM Graph
+        WHERE Current = NEW.Current
+    )
+    WHERE Current = NEW.Current;
+END;
+
+CREATE TRIGGER Probability_on_update
+AFTER UPDATE ON Graph
+FOR EACH ROW
+BEGIN
+    -- Update the probability for all rows with the same current node
+    UPDATE Graph
+    SET Probability = Edge / (
+        SELECT SUM(Edge)
+        FROM Graph
+        WHERE Current = NEW.Current
+    )
+    WHERE Current = NEW.Current;
+END;
