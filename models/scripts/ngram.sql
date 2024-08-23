d02eff65a59eabd9c168451c559b02c00ba795dc
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-08-23 21:19:44 +1000

    Decided to removed triggers as redundant operation

--- models/scripts/ngram.sql
+++ models/scripts/ngram.sql
@@ -1,39 +1,10 @@
+DROP TABLE IF EXISTS Graph;
+
 --Table that stores substring states as an adjacency list
 CREATE TABLE IF NOT EXISTS Graph (
     Current TEXT,
-    previous TEXT,
+    Previous TEXT,
     Edge INTEGER,
-    Probability REAL(2),
-    PRIMARY KEY (Current, previous)
+    Probability REAL,
+    PRIMARY KEY (Current, Previous)
 );
-
-DROP TRIGGER IF EXISTS Probability_on_insert;
-DROP TRIGGER IF EXISTS Probability_on_update;
-
-CREATE TRIGGER Probability_on_insert
-AFTER INSERT ON Graph
-FOR EACH ROW
-BEGIN
-    -- Update the probability for all rows with the same current node
-    UPDATE Graph
-    SET Probability = Edge / (
-        SELECT SUM(Edge)
-        FROM Graph
-        WHERE Current = NEW.Current
-    )
-    WHERE Current = NEW.Current;
-END;
-
-CREATE TRIGGER Probability_on_update
-AFTER UPDATE ON Graph
-FOR EACH ROW
-BEGIN
-    -- Update the probability for all rows with the same current node
-    UPDATE Graph
-    SET Probability = Edge / (
-        SELECT SUM(Edge)
-        FROM Graph
-        WHERE Current = NEW.Current
-    )
-    WHERE Current = NEW.Current;
-END;
