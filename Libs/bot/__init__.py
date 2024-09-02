f8c4d6df8ab5bd1bc9f679f1070c99fb75e2646f
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-09-03 00:02:05 +1000

    please don't be cringe

--- Libs/bot/__init__.py
+++ Libs/bot/__init__.py
@@ -1,5 +1,14 @@
 print("Importing bot dependencies...")
 
+# Packages
 import asyncio
 import discord
+from discord import Intents
+from discord import Message
+from discord import Interaction
+from discord import app_commands
 from discord.app_commands import CommandTree
+
+# Modules
+from Config import config
+from Commands.Interface.interface import *
